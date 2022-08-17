ac023bd49761b2246225167ec8b8d27adec7a80c
rhrazib <rokibulhasancse@gmail.com>
2022-08-17 11:27:28 +0600

    init

---
+++ app/src/main/java/com/example/androidfragment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
